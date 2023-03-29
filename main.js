7da3bc811b38ef4961e5bdf733875b091ff501e7
Wiseman11 <manwise047@gmail.com>
2023-03-29 13:02:10 +0300

    added service page in a branch called services

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multi-text",{
+    strings:["MOBILE APP DEVELOPMENT","WEBSITE DEVELOPMENT","INTERNET OF THINGS","GRAPHIC DESIGN"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
